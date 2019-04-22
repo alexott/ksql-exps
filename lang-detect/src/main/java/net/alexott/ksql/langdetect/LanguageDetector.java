e6577fb778926e01d3cc6da6ebbab8248c88b46c
Alex Ott <alexey.ott@datastax.com>
2019-04-22 22:01:12 +0200

    Use Logger instead of println

--- lang-detect/src/main/java/net/alexott/ksql/langdetect/LanguageDetector.java
+++ lang-detect/src/main/java/net/alexott/ksql/langdetect/LanguageDetector.java
@@ -3,11 +3,14 @@ package net.alexott.ksql.langdetect;
 import com.github.jfasttext.JFastText;
 import io.confluent.ksql.function.udf.Udf;
 import io.confluent.ksql.function.udf.UdfDescription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 
 @UdfDescription(name = "langdetect", description = "Language detection using FastText")
 public class LanguageDetector {
+    private static Logger LOGGER = LoggerFactory.getLogger(LanguageDetector.class);
     private final static String UNKNOWN_LANGUAGE="unk";
     private final static String LABEL_PREFIX="__label__";
 
@@ -25,8 +28,7 @@ public class LanguageDetector {
                 return UNKNOWN_LANGUAGE;
             }
         } catch (Exception ex) {
-            // TODO: use Logger
-            System.err.println("Exception during language detection: " + ex.getMessage());
+            LOGGER.error("Exception during language detection. " + ex);
             return null;
         }
     }
--- lang-detect/src/main/java/net/alexott/ksql/langdetect/LanguageModelHolder.java
+++ lang-detect/src/main/java/net/alexott/ksql/langdetect/LanguageModelHolder.java
@@ -3,24 +3,26 @@ package net.alexott.ksql.langdetect;
 import com.github.jfasttext.JFastText;
 import io.confluent.ksql.function.udf.Udf;
 import io.confluent.ksql.function.udf.UdfDescription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 
 class LanguageModelHolder {
+    private static Logger LOGGER = LoggerFactory.getLogger(LanguageModelHolder.class);
     static JFastText langModel = null;
 
     static {
         try {
+            // TODO: use system property to load compressed or full language model...
             InputStream is = LanguageModelHolder.class.getResourceAsStream("/lid.176.ftz");
             if (is != null) {
                 langModel = new JFastText(is);
             } else {
-                System.err.println("Can't load resource from classpath...");
+                LOGGER.error("Can't load resource from classpath...");
             }
         } catch (Exception ex) {
-            // TODO: use Logger
-            System.err.println("Error loading language model: " + ex.getMessage());
-            ex.printStackTrace(System.err);
+            LOGGER.error("Error loading language model: " + ex.getMessage(), ex);
         }
     }
 
